b9a544f636e553c64ebf77bf159e9095933c8e46
jonathanwoodard <jwoodard@gmail.com>
2015-03-23 18:32:59 -0700

    add some code to the string.py module

--- students/JonWoodard/session02/string.py
+++ students/JonWoodard/session02/string.py
@@ -4,16 +4,29 @@ def string1():
     Things about strings.
     """
     # put some code here
+    a = "some random text"
+    b = []
+    if len(a) > 7:
+        b.append(a[2:5])
+    return b
+
 
 def string2():
     """
     More things about strings.
     """
     # put some more code here
+    another_string = "oh, boy, how about some more random text"
+    another_string.remove("random")
+
 
 def string3():
     """
     All you ever wanted to know about strings.
     """
     # another block of code here
-    
+    one_final_string = "this string has"
+    one_final_string.append = " even more text"
+    print one_final_string
+
+
