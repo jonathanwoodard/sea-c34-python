72b1d13ddd9b11bb55b136ac7d44dc9b3a1369b4
jonathanwoodard <jwoodard@gmail.com>
2015-03-24 18:11:37 -0700

    append new fruit to list of fruit

--- students/JonWoodard/session03/list_lab.py
+++ students/JonWoodard/session03/list_lab.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 
-fruit = ["apples", "pears", "oranges", "peaches"]
+fruit = ["apples", "pears", "oranges", "peachesnew"]
+print(fruit)
+new_fruit = raw_input("Please enter a new fruit-->")
+fruit.append(new_fruit)
+print(fruit)
+fruit_number = raw_input("Please enter a number-->")
