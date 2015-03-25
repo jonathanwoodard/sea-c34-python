4cd6aa012dd379171ca218d422db0c2b2720a5ce
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 03:50:49 -0700

    create mail_room.py

---
+++ students/JonWoodard/mail_room.py
@@ -0,0 +1,10 @@
+def mail_room(name, amount, action):
+    """
+    This will define a series of actions to be taken in the mail room.
+    Args:
+        name: this is the name of the donor, which will be stored in a list
+        amount: this is the amount donated
+        action: this is the action to take; thank you, reciept, etc.
+    Return:
+        print a thank you or reciept for each name in the list, for the amount.
+    """
