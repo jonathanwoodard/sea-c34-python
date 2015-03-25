8afe7def663b693ba8eb7e050ea99b65215589b6
jonathanwoodard <jwoodard@gmail.com>
2015-03-24 22:57:36 -0700

    remove some fruit from the list

--- students/JonWoodard/session03/list_lab.py
+++ students/JonWoodard/session03/list_lab.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
-fruit = ["apples", "pears", "oranges", "peaches"]
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 print(fruit)
 new_fruit = raw_input("Please enter a new fruit-->")
 fruit.append(new_fruit)
 print(fruit)
 fruit_number = int(raw_input("Please enter a number-->"))
 # need to loop this until a suitable value is entered
-# this does not seem to work
+# this does not seem to work - asks all the time
 # if IndexError:
 #     fruit_number = int(raw_input("Try a smaller number-->"))
 print(fruit_number, fruit[fruit_number - 1])
-fruit = ["pomegranites"] + fruit
+fruit = ["Pomegranites"] + fruit
 print(fruit)
-fruit.insert(0, "pineapples")
+fruit.insert(0, "Pineapples")
 print(fruit)
+for item in fruit:
+    if item[0] == "P":
+        print(item)
