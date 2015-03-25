410d72526293ce17fcee507c38c28ce2650f4980
jonathanwoodard <jwoodard@gmail.com>
2015-03-24 22:12:25 -0700

    add more fruit to the beginning of the list

--- students/JonWoodard/session03/list_lab.py
+++ students/JonWoodard/session03/list_lab.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 fruit = ["apples", "pears", "oranges", "peaches"]
 print(fruit)
@@ -7,6 +7,11 @@ fruit.append(new_fruit)
 print(fruit)
 fruit_number = int(raw_input("Please enter a number-->"))
 # need to loop this until a suitable value is entered
-if IndexError:
-    fruit_number = int(raw_input("Try a smaller number-->"))
+# this does not seem to work
+# if IndexError:
+#     fruit_number = int(raw_input("Try a smaller number-->"))
 print(fruit_number, fruit[fruit_number - 1])
+fruit = ["pomegranites"] + fruit
+print(fruit)
+fruit.insert(0, "pineapples")
+print(fruit)
