620e11df6de574dc9eee4c3e7f83b2b3381a700c
jonathanwoodard <jwoodard@gmail.com>
2015-03-24 21:00:51 -0700

    ask again if index error

--- students/JonWoodard/session03/list_lab.py
+++ students/JonWoodard/session03/list_lab.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-fruit = ["apples", "pears", "oranges", "peachesnew"]
+fruit = ["apples", "pears", "oranges", "peaches"]
 print(fruit)
 new_fruit = raw_input("Please enter a new fruit-->")
 fruit.append(new_fruit)
 print(fruit)
-fruit_number = raw_input("Please enter a number-->")
-print(fruit_number)
-if fruit_number >= len(fruit):
-    print(fruit[fruit_number])
-else:
-    print("We don't have that many kinds of fruit. Enter a smaller number")
+fruit_number = int(raw_input("Please enter a number-->"))
+# need to loop this until a suitable value is entered
+if IndexError:
+    fruit_number = int(raw_input("Try a smaller number-->"))
+print(fruit_number, fruit[fruit_number - 1])
