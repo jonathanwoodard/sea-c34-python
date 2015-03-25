beed2a85f92715701afd1436e5de5d08820c3b33
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 00:39:48 -0700

    code ready for testing

--- students/JonWoodard/session03/list_lab.py
+++ students/JonWoodard/session03/list_lab.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# section 1
 
 fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 print(fruit)
@@ -7,10 +8,11 @@ fruit.append(new_fruit)
 print(fruit)
 fruit_number = int(raw_input("Please enter a number-->"))
 # need to loop this until a suitable value is entered
-# this does not seem to work - asks all the time
-# if IndexError:
-#     fruit_number = int(raw_input("Try a smaller number-->"))
-print(fruit_number, fruit[fruit_number - 1])
+# this only asks once
+if fruit_number > len(fruit):
+    fruit_number = int(raw_input("Try a smaller number-->"))
+else:
+    print(fruit_number, fruit[fruit_number - 1])
 fruit = ["Pomegranites"] + fruit
 print(fruit)
 fruit.insert(0, "Pineapples")
@@ -19,9 +21,38 @@ for item in fruit:
     if item[0] == "P":
         print(item)
 
+# section 2
+
 print(fruit)
 fruit.pop(-1)
 print(fruit)
+fruit_2 = fruit * 2
 remove_fruit = raw_input("Which fruit would you like to remove? -->")
-fruit.remove(remove_fruit)
+if remove_fruit not in fruit_2:
+    print("We don't have that fruit, try again")
+    remove_fruit = raw_input("Which fruit would you like to remove? -->")
+while remove_fruit in fruit_2:
+    fruit_2.remove(remove_fruit)
+print(fruit_2)
 
+# section 3
+
+for item in fruit:
+    like_fruit = raw_input("Do you like " + item.lower + "? -->")
+    while like_fruit != "yes" or "no":
+        print('Please answer "yes" or "no"')
+        like_fruit = raw_input("Do you like " + item.lower + "? -->")
+    if like_fruit == "no":
+        fruit.remove(item)
+print(fruit)
+
+# section 4
+
+fruit_copy = fruit[:]
+for item in fruit_copy:
+    item.reverse
+fruit.pop(-1)
+print(fruit, fruit_copy)
+
+# if __name__ == "__main__":
+# add some assert statements to test
