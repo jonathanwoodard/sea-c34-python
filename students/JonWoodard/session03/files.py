59be6aee05234336bafee77920ea8e6c12e72eeb
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 18:38:29 -0700

    add some stuff to files.py

---
+++ students/JonWoodard/session03/files.py
@@ -0,0 +1,14 @@
+def file_read(quest1):
+    """
+    Define a question about reading files.
+    Create code to answer the question.
+    """
+    # insert code here
+
+def file_write(quest2):
+    """
+    Define a question about writing files.
+    Create code to answer the question.
+    """
+    # insert code here
+    
