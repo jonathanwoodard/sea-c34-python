6875444e6d8473da6dbb3db731ec68bb2c515d34
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 18:15:34 -0700

    add stuff to a dictionary

--- students/JonWoodard/session03/dictionaries.py
+++ students/JonWoodard/session03/dictionaries.py
@@ -1,6 +1,34 @@
-def dictionary_questions(quest1, quest2, quest3)
+def dictionary_1():
     """
-    Define three questions about dictionaries.
+    Define four questions about dictionaries and sets.
+    Quesstion 1:
+        How do you add things to a dictionary?
+    """
+    zoo_animals = ["lions", "tigers", "monkeys"]
+    how_many = [3, 2, 11]
+    in_the_zoo = {}
+
+    in_the_zoo = dict(zip(zoo_animals, how_many))
+    print(in_the_zoo)
+
+def dictionary_2(quest2):
+    """
+    Define another question.
+    Question 2:
+        What types of things can be put in a dictionary?
     """
 
-    
+def dictionary_3(quest3):
+    """
+    Define another question.
+    Question 3:
+        How do you access the items in a dictionary?
+    """
+
+def dictionary_4(quest4):
+    """
+    Define a last question.
+    Question 4:
+        How do you remove things from a dictionary?
+    """
+
