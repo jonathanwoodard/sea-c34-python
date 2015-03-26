296621adf3dc2aac463397f5d5578fc85db1fa2f
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 18:41:29 -0700

    clean up task8

--- students/JonWoodard/session03/file_read_write.py
+++
@@ -1,6 +0,0 @@
-def file_read_write(quest1, quest2, quest3):
-    """
-    Define three questions about reading and writing files.
-    Create code to answer each question.
-    """
-    
--- students/JonWoodard/session03/paths.py
+++ students/JonWoodard/session03/paths.py
@@ -1,6 +1,7 @@
 def paths(quest1):
     """
-    Define one question about paths.
+    Define a question about paths.
     Create some code to answer that question.
     """
+
     
