0ace5b999001b511021a4c44383718f2ba30bfad
jonathanwoodard <jwoodard@gmail.com>
2015-03-25 18:33:30 -0700

    create an exception in exception

--- students/JonWoodard/session03/exeptions.py
+++ students/JonWoodard/session03/exeptions.py
@@ -1,6 +1,24 @@
-def exception_questions(quest1, quest2, quest3):
+def exception_1(quest1):
     """
-    Define three questions about exceptions.
-    Write some code to answer each of these questions.
+    Define one question about exceptions.
+    Question 1:
+        What kind of things will generate exceptions?
     """
+    # try to do something that can't be done
+
+    a = int(raw_input)
+    b = int(raw_input)
+    answer = a / b
+    if b == 0:
+        raise ZeroDivisionError("Division by zero is not defined")
+
+
+def exception_2(quest2):
+    """
+    Define another question about exceptions.
+    Question 2:
+        How do you resolve an exception?
+    """
+    # use else to give it a way out of the exception
+
     
