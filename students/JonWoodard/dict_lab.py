20b1c8ecb2c8dae1ba0c528a66ea0b9735de654e
jonathanwoodard <jwoodard@gmail.com>
2015-03-29 18:08:10 -0700

    complete step 1

--- students/JonWoodard/dict_lab.py
+++ students/JonWoodard/dict_lab.py
@@ -3,5 +3,11 @@ dict_1 = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
 print(dict_1)
 dict_1.pop('cake')
 print(dict_1)
-dict_1[fruit] = 'Mango'
+dict_1['fruit'] = 'Mango'
 print(dict_1)
+keys = dict_1.keys()
+print(keys)
+values = dict_1.values()
+print(values)
+'cake' in dict_1
+'Mango' in values
