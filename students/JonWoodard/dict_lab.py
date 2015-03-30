e22078fbc4e534e5c1855c88cfda2278cda07d16
jonathanwoodard <jwoodard@gmail.com>
2015-03-29 18:48:28 -0700

    complete item 2

--- students/JonWoodard/dict_lab.py
+++ students/JonWoodard/dict_lab.py
@@ -11,3 +11,16 @@ values = dict_1.values()
 print(values)
 'cake' in dict_1
 'Mango' in values
+
+
+# section 2: create lists from 1-15 in base 10 and hex
+list_1 = []
+list_2 = []
+for int in range(16):
+    list_1.append(int)
+    list_2.append(hex(int))
+dict_2 = dict(zip(list_1, list_2))
+print(dict_2)
+
+
+
