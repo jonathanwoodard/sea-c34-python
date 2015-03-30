c3feb26fb885b661b6cb4dde80d3e4dfa6baa8fb
jonathanwoodard <jwoodard@gmail.com>
2015-03-30 16:59:55 -0700

    add questions to functional.py

---
+++ students/JonWoodard/session04/functional.py
@@ -0,0 +1,21 @@
+# submit 3 questions about lambdas and functional programming
+def funct_1():
+    """
+    What does the map function do?
+    """
+
+
+
+def funct_2():
+    """
+    How do you use a filter to get the desired values from a list/dict?
+    """
+
+
+
+def funct_3():
+    """
+    Is there a difference between using map/filter/reduce or comprehensions?
+    Is one preferred over the other?
+    Is it context dependent?
+    """
