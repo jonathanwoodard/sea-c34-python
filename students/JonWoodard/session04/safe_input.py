ed77056b7d43830a8e7c72bb06f0f2a2b5ded53a
jonathanwoodard <jwoodard@gmail.com>
2015-03-30 14:58:43 -0700

    try different code - still doesn't work

--- students/JonWoodard/session04/safe_input.py
+++ students/JonWoodard/session04/safe_input.py
@@ -1,12 +1,13 @@
 def safe_input():
+    
     """
     This function will take raw user input and return 'None'
     rather than raising exceptions.
     """
 
-    out = raw_input('Prompt for user input ->')
-    while out == EOFError:
-        return 'eof'
-    while out == KeyboardInterrupt:
-        return 'int'
-    print(out)
+    raw_input('Prompt for user input ->')
+    if EOFError is True:
+        return 'None'
+    if KeyboardInterrupt is True:
+        return 'None'
+
