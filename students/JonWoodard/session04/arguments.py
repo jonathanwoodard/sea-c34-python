7efddf8e6b4d49cffc434588c17aaccc3a6a0085
jonathanwoodard <jwoodard@gmail.com>
2015-03-30 16:44:41 -0700

    add questions to arguments.py

---
+++ students/JonWoodard/session04/arguments.py
@@ -0,0 +1,18 @@
+# submit 3 questions about arguments
+def arg_1():
+    """
+    How do you define an argument as optional?
+    """
+
+
+def arg_2():
+    """
+    How do you set a default value for an optional argumemnt?
+    """
+
+
+def arg_3():
+    """
+    What is the difference between args and kwargs?
+    Why do they need to be two different things?
+    """
