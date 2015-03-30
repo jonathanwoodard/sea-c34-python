9a511478cd3ebba608f902031b95295dbc9986a6
jonathanwoodard <jwoodard@gmail.com>
2015-03-30 16:50:43 -0700

    add questions to comprehensions.py

---
+++ students/JonWoodard/session04/comprehensions.py
@@ -0,0 +1,17 @@
+# submit 3 questions about comprehensions
+def comp_1():
+    """
+    How do you create a new list using a list comprehension?
+    """
+
+
+def comp_2():
+    """
+    How do you include loops and conditionals in a comprehension?
+    """
+
+
+def comp_3():
+    """
+    How do you create a dictionary with a comprehension?
+    """
