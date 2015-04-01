ee10cfb1cef5862712573169842af1aa6dc9d896
jonathanwoodard <jwoodard@gmail.com>
2015-04-01 15:17:00 -0700

    clean up functions in mail_room.py

--- students/JonWoodard/session04/mail_room.py
+++ students/JonWoodard/session04/mail_room.py
@@ -1,4 +1,97 @@
 #!/usr/bin/python
+# create a dictionary, d = {}
+d = {}
+# donors
+ls1 = ['Felix Hernandez', 'Hisashi Iwakuma', 'Taijuan Walker',
+       'Mike Zunino', 'Robinson Cano', 'Brad Miller', 'Kyle Seager']
+# amounts
+ls2 = [[1700], [900], [1600], [150], [1100], [250], [750]]
+d = dict(zip(ls1, ls2))
+donors = []
+amounts = []
+for key in d:
+    donors.append(key)
+    amounts.append(d[key])
+
+# create a function to prompt user for data
+def safe_input(prompt):
+
+    """
+    This function will take raw user input and return 'None'
+    rather than raising exceptions.
+    """
+    prompt = 'Prompt user for desired input'
+    try:
+        usr_in = raw_input(prompt)
+    except:
+        EOFError or KeyboardInterrupt
+        return 'None'
+    else:
+        return usr_in
+
+
+# create function to ask for donor name
+def donor_name(name):
+    """
+    This function will take user input and create a thank you email
+    """
+    prompt = u'Enter the Full Name of the Donor: ->'
+    name = safe_input(prompt)
+    if name in d:
+        donation_amount(name)
+    else:
+        d.update(name, ([]))
+
+
+def donation_amount(name):
+    """
+    This function will determine whether the value entered is a number
+    If the value is a number, an email will be generated
+    Otherwise it will return the user to the donation_amount function
+    """
+    prompt = u'Please enter the donation amount: ->'
+    amount = safe_input(prompt)
+    if amount is float:
+        if amount in d[name]:
+            email_text()
+    else:
+        print('That does not appear to be a valid number. Try again')
+        donation_amount()
+
+
+def email_text(amount, name):
+    """
+    This function will take the user entered name and donation amount
+    to generate a thank you email.
+
+    """
+    msg = u"Dear {**}, The Royal Society for Putting Things on top of" \
+        "Other Things would like to sincerely thank you for your generous" \
+        "donation of ${*}. This kind donation will enable us to put many" \
+        "more Things on top of Other Things in the coming year."\
+        .format(amount, name)
+    print(msg)
+
+
+def report():
+    """
+    This function will generate a report of donors and donations.
+    The report will contain 'name', total of all donations, number
+    of donations, and the average donation value.
+    The report will be sorted by the donation total, and formatted so 
+    that the values are neatly alligned in rows.
+    """
+    report_list = []
+    for key in d:
+        tot = sum(d[key])
+        n = len(d[key])
+        average = tot / n
+        report_entry = (d[key], tot, n, average)
+        report_list.append(report_entry)
+    report_list.sort(2)
+    print(report_list)
+
+
 def mail_room(user_input):
     """
     This will define a series of actions to be taken in the mail room.
@@ -13,92 +106,23 @@ def mail_room(user_input):
         "Create a Report" generates a formated list
         "list" prints a list of names and returns to original prompt
     """
-# create a dictionary, d = {}
-d = {}
-# donors
-ls1 = ['Felix Hernandez', 'Hisashi Iwakuma', 'Taijuan Walker', 'Mike Zunino', 'Robinson Cano', 'Brad Miller', 'Kyle Seager']
-ls2 = [1700, 900, 1600, 150, 1100, 250, 750] # amounts
-d = dict(zip(ls1, ls2))
-keys = d.keys
-values = d.values
+    print(u'Please select from the following options:')
+    print(u'To send a thank you, enter "1"')
+    print(u'To Create a Report, enter "2"')
+    print(u'To see a list of donors, press "L"')
+    print(u'To exit, press "Q"')
+    prompt = u'Please make your selection now: ->'
+    user_input = safe_input(prompt)
+    if user_input == 1:
+        donor_name()
+    elif user_input == 2:
+        report()
+    elif user_input.lower == 'l':
+        print(donors)
+        mail_room(user_input)
+    elif user_input.lower == 'q':
+        break
 
-
-def safe_input():
-
-    """
-    This function will take raw user input and return 'None'
-    rather than raising exceptions.
-    """
-
-    raw_input('% ->')
-    try:
-        return (raw_input())
-    except:
-        EOFError or KeyboardInterrupt
-        return 'None'
-
-
-def thank_you():
-    """
-    This function will take user input and create a thank you email
-    """
-    safe_input(u'Enter the Full Name of the Donor: ->')
-    if raw_input in d[keys]:
-        donation_amount()
-    else:
-        d.update[keys]
-        donation_amount()
-
-
-
-def donation_amount():
-    """
-    This function will take user input for donation amount and add it to d
-    """
-    safe_input(u'Please enter the donation amount: ->')
-
-
-def number_input():
-    """
-    This function will determine whether the value entered is a number
-    If the value is a number, an email will be generated
-    Otherwise it will return the user to the donation_amount function
-    """
-    if donation_amount.raw_input() is float:
-        email_text()
-    else:
-        donation_amount()
-
-
-
-def email_text():
-    """
-    This function will take the user entered name and donation amount
-    to generate a thank you email.
-
-    """
-    print(u"Dear %, The Royal Society for Putting Things on top of Other Things")
-    print(u"would like to sincerely thank you for your generous donation of %.")
-    print(u"This kind donation will enable us to put many more Things on top of ")
-    print(u"Other Things in the coming year.")
-
-
-def report():
-    """
-    This function will generate a report of donors and donations.
-    The report will contain 'name', total of all donations, number
-    of donations, and the average donation value.
-    The report will be sorted by the donation total, and formatted so 
-    that the values are neatly alligned in rows.
-    """
-
-    for key in d:
-        tot = sum(d[values])
-        n = len(d[values])
-        average = tot / n
-        report_entry = [d[key], tot, n, average]
-        print(report_entry.sort(2))
-        
 # if raw_input == 'q'
 #   call function(prompt)
 # def prompt(raw_input)
