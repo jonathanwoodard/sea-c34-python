0429f545e9598e690c4525e537a2e2e194670f87
jonathanwoodard <jwoodard@gmail.com>
2015-04-06 10:26:57 -0700

    create dict "food_prefs"

---
+++ students/JonWoodard/session05/dict_comp.py
@@ -0,0 +1,7 @@
+food_prefs = {"name": u"Jon",
+              u"city": u"Seattle",
+              u"cake": u"german chocolate",
+              u"fruit": u"mango",
+              u"salad": u"caprese",
+              u"pasta": u"ravioli"}
+
