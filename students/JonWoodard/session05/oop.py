9c9d9779bf9ccbf4442d69151845285fcb72dc81
jonathanwoodard <jwoodard@gmail.com>
2015-04-06 16:34:10 -0700

    create oop.py

---
+++ students/JonWoodard/session05/oop.py
@@ -0,0 +1,15 @@
+"""
+This file will contain two questions about object oriented progamming.
+"""
+
+
+def oop_1():
+    """
+    First question:
+    """
+
+
+def oop_2():
+    """
+    Second question:
+    """
