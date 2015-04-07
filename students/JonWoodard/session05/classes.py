82e0fd8e2f1f26dc96a5475a894f34b83d7bef3f
jonathanwoodard <jwoodard@gmail.com>
2015-04-06 18:14:59 -0700

    create classes.py and subclasses.py

---
+++ students/JonWoodard/session05/classes.py
@@ -0,0 +1,10 @@
+"""
+Ask one question about classes in python
+"""
+
+
+def classes_1():
+    """
+    One question:
+    """
+    
---
+++ students/JonWoodard/session05/subclasses.py
@@ -0,0 +1,27 @@
+"""
+Ask four questions about sublassing
+"""
+
+
+def subclass_1():
+    """
+    First question about subclasses:
+    """
+
+
+def subclass_2():
+    """
+    Second question about subclasses:
+    """
+
+
+def subclass_3():
+    """
+    Third question about subclasses:
+    """
+
+
+def subclass_4():
+    """
+    Fourth question about subclasses:
+    """
