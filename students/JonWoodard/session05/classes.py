10266312e382e4d4310bf8a7ec9435dc6dc04fb3
jonathanwoodard <jwoodard@gmail.com>
2015-04-06 23:43:34 -0700

    add som comments to classes.py

--- students/JonWoodard/session05/classes.py
+++ students/JonWoodard/session05/classes.py
@@ -6,5 +6,7 @@ Ask one question about classes in python
 def classes_1():
     """
     One question:
+    How do you create a new class, and what are the minimum requirements
+    of that class?
     """
     
