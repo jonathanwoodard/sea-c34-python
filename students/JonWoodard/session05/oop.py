d3f3cdd79d9fe14d75eb2376b7f119a844014e16
jonathanwoodard <jwoodard@gmail.com>
2015-04-06 18:07:21 -0700

    add a question/comment

--- students/JonWoodard/session05/oop.py
+++ students/JonWoodard/session05/oop.py
@@ -6,6 +6,7 @@ This file will contain two questions about object oriented progamming.
 def oop_1():
     """
     First question:
+    I am not really clear on what makes something object oriented.
     """
 
 
