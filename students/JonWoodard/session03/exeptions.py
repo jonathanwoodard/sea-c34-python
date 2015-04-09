cb4456395325ac1b909e0e462408a5ac479031ed
jonathanwoodard <jwoodard@gmail.com>
2015-04-08 18:20:41 -0700

    finish exceptions.py

--- students/JonWoodard/session03/exeptions.py
+++ students/JonWoodard/session03/exeptions.py
@@ -1,4 +1,4 @@
-def exception_1(quest1):
+def exception_1():
     """
     Define one question about exceptions.
     Question 1:
@@ -6,19 +6,30 @@ def exception_1(quest1):
     """
     # try to do something that can't be done
 
-    a = int(raw_input)
-    b = int(raw_input)
-    answer = a / b
-    if b == 0:
-        raise ZeroDivisionError("Division by zero is not defined")
+    a = float(raw_input("Enter a number:\n-> "))
+    b = float(raw_input("Enter another number:\n-> "))
+    try:
+        answer = a / b
+    except:
+        if b == 0:
+            raise ZeroDivisionError("Division by zero is not defined")
+    print(answer)
 
 
-def exception_2(quest2):
+def exception_2():
     """
     Define another question about exceptions.
     Question 2:
         How do you resolve an exception?
     """
     # use else to give it a way out of the exception
-
-    
+    a = float(raw_input("Enter a number:\n-> "))
+    b = float(raw_input("Enter another number:\n-> "))
+    try:
+        answer = a / b
+    except:
+        if b == 0:
+            print("Division by zero is not defined")
+            b = float(raw_input("Enter another number:\n-> "))
+            answer = a / b
+    print(answer)
