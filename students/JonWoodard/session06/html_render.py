9e39e800e0512cde709ba9df7cfbdf0bab397acd
jonathanwoodard <jwoodard@gmail.com>
2015-04-12 14:32:01 -0700

    first commit for part 2

--- students/JonWoodard/session06/html_render.py
+++ students/JonWoodard/session06/html_render.py
@@ -5,6 +5,7 @@ Python class example.
 
 """
 
+
 # The start of it all:
 # Fill it all in here.
 class Element(object):
@@ -24,10 +25,21 @@ class Element(object):
     def render(self, file_out, ind=""):
         """Render the tag and strings in content."""
         output = (
-            u"{indent}<{tag}>\n")
+            u"{indent}<{tag}>\n"
             "{indent}{content}"
             "{indent}<{tag}>"
-            .format(indent=ind, tag=self.tag, content=self.content)
-        )
+            ).format(indent=ind, tag=self.tag, content=self.content)
+
         file_out.write(output)
 
+
+class Html(Element):
+    tag = u"html"
+
+
+class Body(Element):
+    tag = u"body"
+
+
+class P(Element):
+    tag = u"p"
