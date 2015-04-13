013991894e0b6ca3195a8807c02f44c279a9f7a6
jonathanwoodard <jwoodard@gmail.com>
2015-04-13 15:19:52 -0700

    add some code to classes.py

--- students/JonWoodard/session05/classes.py
+++ students/JonWoodard/session05/classes.py
@@ -3,10 +3,16 @@ Ask one question about classes in python
 """
 
 
-def classes_1():
+def farm_animal(object):
     """
     One question:
-    How do you create a new class, and what are the minimum requirements
-    of that class?
+    How do you create a new class?
     """
-    
+    def __init__(self, name, sound):
+        """Initialize the class."""
+        self.name = name
+        self.sound = sound
+
+    def basic_info(self):
+        """Return the name and sound of the farm_animal."""
+        print("The {name} says {sound}.".format(self.name, self.sound))
