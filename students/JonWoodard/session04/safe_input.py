09185872a7a197fa31f9bd7a3f544d8e394f3b23
jonathanwoodard <jwoodard@gmail.com>
2015-04-12 22:53:25 -0700

    code now works as expected

--- students/JonWoodard/session04/safe_input.py
+++ students/JonWoodard/session04/safe_input.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
-def safe_input():
+def safe_input(usr_in=None):
 
     """
     This function will take raw user input and return 'None'
     rather than raising exceptions.
     """
 
-    raw_input('Please input a value ->')
     try:
-        return (raw_input())
-    except:
-        EOFError or KeyboardInterrupt
+        usr_in = raw_input('Please input a value:\n -> ')
+        return usr_in
+    except (EOFError, KeyboardInterrupt):
         return 'None'
 
+safe_input()
