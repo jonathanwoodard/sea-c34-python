7a60ee37a628b7591f7a42ae7bc2536d0436da72
jonathanwoodard <jwoodard@gmail.com>
2015-04-13 10:40:47 -0700

    complete arguments.py

--- students/JonWoodard/session04/arguments.py
+++ students/JonWoodard/session04/arguments.py
@@ -1,18 +1,34 @@
 # submit 3 questions about arguments
-def arg_1():
+def arg_1(opt=2):
     """
-    How do you define an argument as optional?
+    How do you define an optional argument?
     """
+    result = 5 ** opt
+    print(result)
+
+arg_1()
+arg_1(3)
 
 
-def arg_2():
+def arg_2(**answer):
     """
-    How do you set a default value for an optional argumemnt?
+    What is an example of a function using kwargs?
     """
+    answer = (raw_input(u"What do you want to eat?\n-> "))
+    print("{}".format(answer))
+
+arg_2()
 
 
-def arg_3():
+def arg_3(*enthusiasm, **answer):
     """
-    What is the difference between args and kwargs?
-    Why do they need to be two different things?
+    What is an example of a function using both args and kwargs?
     """
+    answer = (raw_input(u"What do you want to eat?\n-> "))
+    enthusiasm = int(raw_input(
+        u"How much do you want?\n"
+        u"(This must be an integer)\n -> ")
+        )
+    print("{}!".format(enthusiasm * answer))
+
+arg_3()
