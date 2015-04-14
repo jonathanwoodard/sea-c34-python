7a4531818e0c0d09e61b5d378b669149a928c844
jonathanwoodard <jwoodard@gmail.com>
2015-04-14 10:40:37 -0700

    complete test for series.py

---
+++ students/JonWoodard/session07/test_series.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# import pytest  # used for the exception testing
+from series import fibonacci
+from series import lucas
+
+
+def test_fib(n):
+
+    assert fibonacci(2) == 1
+    assert fibonacci(7) == 8
+    assert fibonacci(27) == 121393
+
+
+def test_luc(n):
+
+    assert lucas(2) == 1
+    assert lucas(7) == 18
+    assert lucas(27) == 271443
