fedd1f193e1c5719a538ad3e3e02f5754ac26c62
jonathanwoodard <jwoodard@gmail.com>
2015-04-13 20:19:46 -0700

    complete part 2

--- students/JonWoodard/session06/html_render.py
+++ students/JonWoodard/session06/html_render.py
@@ -10,35 +10,52 @@ Python class example.
 # Fill it all in here.
 class Element(object):
     """An HTML element."""
-    tag = u""
-    indent = u" "
+    tag = u"html"
+    indent = u"     "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
+        self.children = [content] if content else []
+        # self.content = self.indent + str(self.content) if content else ""
 
-    def append(self, string):
+    def append(self, content):
         """Append string to content."""
-        self.content += (
-            u"{indent}{str}".format(indent=self.indent, str=str(string))
-            )
+        self.children.append(content)
+        # self.content += (
+        #    u"{indent}{str}\n".format(indent=self.indent, str=str(string))
+        #    )
 
     def render(self, file_out, ind=""):
         """Render the tag and strings in content."""
-        output = (
-            u"{indent}<{tag}>"
-            "{indent}{content}"
-            ).format(indent=ind, tag=self.tag, content=self.content)
+        # output = (
+        #    u"{indent}<{tag}>\n"
+        #    "{indent}{content}"
+        #    "{indent}<{tag}>"
+        #    ).format(indent=ind, tag=self.tag, content=self.content)
 
-        file_out.write(output)
+        file_out.write(u"{indent}\n".format(indent=ind))
+        for child in self.children:
+            try:
+                child.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write(
+                    u"{indent}{child}\n".format(
+                        indent=self.indent, child=unicode(child))
+                    )
+        file_out.write(u"{indent}\n".format(indent=ind))
 
 
-# class Html(Element):
-#    tag = u"html"
+class Html(Element):
+    header = u"<!DOCTYPE html>\n"
+    tag = u"html"
+
+    def render(self, file_out, ind=u""):
+        # file_out.write(self.header)
+        Element.render(self, file_out, ind)
 
 
-# class Body(Element):
-#    tag = u"body"
+class Body(Element):
+    tag = u"body"
 
 
-# class P(Element):
-#    tag = u"p"
+class P(Element):
+    tag = u"p"
