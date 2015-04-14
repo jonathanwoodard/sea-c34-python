d4e43dd8b7f55b341849a9d9378bbde15f34d321
jonathanwoodard <jwoodard@gmail.com>
2015-04-14 05:07:00 -0700

    complete classes.py

--- students/JonWoodard/session05/classes.py
+++ students/JonWoodard/session05/classes.py
@@ -3,7 +3,7 @@ Ask one question about classes in python
 """
 
 
-def farm_animal(object):
+class FarmAnimal(object):
     """
     One question:
     How do you create a new class?
@@ -15,4 +15,12 @@ def farm_animal(object):
 
     def basic_info(self):
         """Return the name and sound of the farm_animal."""
-        print("The {name} says {sound}.".format(self.name, self.sound))
+        print("The {} says {}.".format(self.name, self.sound))
+
+if __name__ == "__main__":
+    a1 = FarmAnimal("cow", "moo")
+    a2 = FarmAnimal("pig", "oink")
+    a3 = FarmAnimal("duck", "quack")
+    animals = [a1, a2, a3]
+    for a in animals:
+        a.basic_info()
