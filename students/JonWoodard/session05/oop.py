e8d3ba6983a4bf0d8e2c6d7b2010b21df4c0ecbe
jonathanwoodard <jwoodard@gmail.com>
2015-04-14 08:41:22 -0700

    complete oop.py

--- students/JonWoodard/session05/oop.py
+++ students/JonWoodard/session05/oop.py
@@ -3,14 +3,40 @@ This file will contain two questions about object oriented progamming.
 """
 
 
+class Animal(object):
+    """A new class called 'Animal'."""
+
+    def __init__(self, name, sound):
+        """Initialize the class."""
+        self.name = name
+        self.sound = sound
+
+    def basic_info(self):
+        """Return the name and sound of the animal."""
+        print("The {} says {}.".format(self.name, self.sound))
+
+
 def oop_1():
     """
     First question:
-    I am not really clear on what makes something object oriented.
+    What kinds of things are objects?
+    Are classes objects? Are functions objects? Are variables objects?
     """
+    print(type(Animal.__init__))
+    print(type(Animal.basic_info))
+    print(type(Animal))
 
 
 def oop_2():
     """
     Second question:
+    Are different instances of an object different objects?
     """
+
+    a1 = Animal("cow", "moo")
+    a2 = Animal("pig", "oink")
+    a3 = Animal("duck", "quack")
+    animals_1 = [a1, a2, a3]
+    animals_2 = [a1, a2, a3]
+
+    assert animals_1 == animals_2
