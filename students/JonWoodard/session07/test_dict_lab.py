323119f3e5e955a9e980d6a0a776899115825ee6
jonathanwoodard <jwoodard@gmail.com>
2015-04-14 12:03:49 -0700

    complete test for dict_lab.py

---
+++ students/JonWoodard/session07/test_dict_lab.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# import pytest  # used for the exception testing
+from dict_lab import dict_fun_1
+
+def dict_1_test():
+
+    assert "name" in dict_1
+    assert "cake" not in dict_1
+    assert "Mango" in values
