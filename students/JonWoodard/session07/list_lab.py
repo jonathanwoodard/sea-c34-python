cbdf8874ed9693ef3bd484a0b45b19c41de13f78
jonathanwoodard <jwoodard@gmail.com>
2015-04-18 13:54:17 -0700

    fix error in list_lab.py

--- students/JonWoodard/session07/list_lab.py
+++ students/JonWoodard/session07/list_lab.py
@@ -3,7 +3,7 @@
 
 master_fruit = ["Apples", "Pears", "Oranges", "Peaches"]
 
-
+"""
 def fruit_1():
     fruit = master_fruit[:]
     print(fruit)
@@ -26,6 +26,7 @@ def fruit_1():
         if item[0] == "P":
             print(item)
 
+
 # section 2
 fruit = master_fruit[:]
 print(fruit)
@@ -44,6 +45,7 @@ print(fruit_2)
 
 # section 3
 
+
 copy = fruit[:]
 for item in fruit:
     lower = item.lower()
@@ -56,11 +58,12 @@ for item in fruit:
     if like_fruit == "no":
         copy.remove(item)
 print(copy)
+"""
+
 
 # section 4
-
-
 def fruit_2():
+    # fruit_copy = []
     fruit_copy = master_fruit[:]
     for item in master_fruit:
         newitem = item[::-1]
@@ -68,8 +71,9 @@ def fruit_2():
             i = fruit_copy.index(item)
             fruit_copy[i] = newitem
     fruit_copy.pop(-1)
+    return fruit_copy
     print(master_fruit, fruit_copy)
-fruit_2()
+# fruit_2()
 
 # if __name__ == "__main__":
 # add some assert statements to test
